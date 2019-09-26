c475b037c2380b50b5ff1597e2d158020d67a317
tutukaka <dikstasisb12@gmail.com>
2019-09-26 17:31:34 +0300

    скопирован проект созданый на уроке

--- js/js.js
+++ js/js.js
@@ -1,2 +1,37 @@
 "use strict";
-alert ('hello world');
+
+class ItemsList {
+    constructor() {
+        this.items = [];
+    }
+
+    fetchItems() {
+        this.items = [
+    { title: 'Компьютерная мышь', price: 400, img: 'mouse.jpg' },
+    { title: 'Жесткий диск SSD 480Gb', price: 10000, img: 'ssd.jpg' },
+    { title: 'Материнская плата', price: 4000, img: 'motherboard.jpg' },
+    { title: 'Видео-карта', price: 15000, img: 'video.jpg' },
+]}
+
+    render() {
+        return this.items.map((item) => new Item(item.title, item.price, item.img).render()).join('');
+    }
+}
+
+class Item {
+    constructor(title, price, image) {
+        this.price = price;
+        this.title = title;
+        this.img = image;
+    }
+
+    render() {
+        return `<div class="catalog__item bxbb"> <img src="image/${this.img}" alt="photo"><h3>${this.title}</h3>
+<p>цена: ${this.price}р</p><div class="button catalog_button">добавить</div></div>`
+    }
+}
+
+const items = new ItemsList();
+items.fetchItems();
+
+document.querySelector('.catalog').innerHTML = items.render();
