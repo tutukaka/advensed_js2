31484a3a77094e38e2393bf3e4dd7972d9f57965
tutukaka <dikstasisb12@gmail.com>
2019-10-07 17:18:29 +0300

    доделал корзину

--- js/js.js
+++ js/js.js
@@ -1,34 +1,20 @@
 "use strict";
 
-function sendRequest(url) {
-    return new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
-        xhr.open('GET', url);
-
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === XMLHttpRequest.DONE) {
-                if (xhr.status !== 200) {
-                    reject();
-                }
-                const users = JSON.parse(xhr.responseText);
-
-                resolve(users);
-            }
-        };
-        xhr.send();
-    });
-}
-
 class ItemsList  {
     constructor() {
         this.items = [];
+        // this.loaded = true;
+        // this.filteredItems = items;
     }
 
     fetchItems() {
-        return sendRequest('/goods')
+        return fetch('/goods')
+            .then(response => response.json())
             .then((items) => {
                 this.items = items;
-            });
+                // this.loaded = true;
+                // this.filteredItems = items;
+            })
     }
 
     render() {
@@ -56,59 +42,129 @@ const items = new ItemsList ();
 const catalog = document.querySelector('.catalog');
 items.fetchItems().then(() => {
     catalog.innerHTML = items.render();
-    const test = document.getElementsByClassName('catalog_button');
-    for (let i =0; i<test.length; i++){
-        test[i].addEventListener('click', test2);
-    }
 });
 
 class BasketList  {
     constructor() {
         this.basket = [];
 
-        this._output = null
+        this._output = null;
+    }
+
+    //запрашивает с сервера наличие товаров в корзине
+    fetchBasket() {
+        return fetch('/cart')
+            .then(response => response.json())
+            .then((item) => {
+                this.basket = item;
+            })
     }
 
     checkItems(art) {
         // проверяет есть ли в корзине этот товар
         for (let i = 0; i < this.basket.length; i++) {
-            if (this.basket[i].id === art) {
+            if (this.basket[i].id === art.id) {
                 //если есть добавляет количество товара
-                this.basket[i].quantity++;
-                return this.render();
+                this.addQuantityAdd(i);
+                this.updateQuantityServer(art.id, i);
+                return this.renderQuantity(i, art.id)
             }
         }
-        //если нет добавляет обект в корзину
+        //если нет добавляет товар в корзину
         this.addItem(art);
-        console.log(basket);
-        return this.render();
+        return basketOut.innerHTML = this.render();
     }
 
+    //добавляет новый товар в корзину
     addItem(content) {
-        // this.basket.push(content)
-        return sendRequest(`/goods/${content}`).then((basket) => {
-            this.basket.push(basket);
-        });
+        this.basket.push(content);
+        this.addItemServer(content)
     }
 
+    //добавляет новый товар на сервер
+    addItemServer(content){
+        fetch(`cart`, {
+            method: 'POST',
+            body: JSON.stringify(content),
+            headers: {
+                'Content-type': 'application/json',
+            },
+        })
+    }
+
+    //добавляет количество товара в корзине
+    addQuantityAdd(i){
+        this.basket[i].quantity++;
+    }
+
+    //уменьшает количество товара в корзине
+    addQuantityDiminish(art){
+        for (let i = 0; i < this.basket.length; i++) {
+            if (this.basket[i].id === art) {
+                if (this.basket[i].quantity > 1) {
+                    this.basket[i].quantity--;
+                    this.updateQuantityServer(art, i);
+                    this.renderQuantity(i, art);
+                    return false
+                } else if (confirm('Вы действительно хотите удалить товар из корзины?')) {
+                    return this.delete (i, art);
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
+
+    //обновляет количество товара на сервере
+    updateQuantityServer(id, i){
+        fetch(`/cart/${id}`, {
+            method: 'PATCH',
+            body: JSON.stringify({quantity: this.basket[i].quantity}),
+            headers: {
+                'Content-type': 'application/json',
+            },
+        })
+    }
+
+    renderQuantity(i, art){
+        const quantityOut = document.getElementsByClassName('quantity_out');
+        for (let k = 0; k < quantityOut.length; k++) {
+            if (+quantityOut[k].parentNode.dataset.art === art) {
+                return quantityOut[k].innerHTML = this.basket[i].quantity;
+            }
+        }
+    }
+
+    //удаляет товар из корзины, с сервера, со страницы
+    delete(j, id){
+        this.basket.splice(j, 1);
+        fetch(`/cart/${id}`, {
+            method: 'DELETE',
+        }).then(response => response.json());
+        return basketOut.innerHTML = this.render()
+    }
+
+    //передает необходимые параметры для вывода корзины на страницу
     render() {
         return this.basket.map((item) => new ItemBasket(item.title, item.price, item.img, item.id, item.quantity)
             .render()).join('');
     }
 
+    //вывод общей стоимости товаров на страницу
     outResult() {
         if (!this._output) {
             this._output = document.createElement("div");
             this._output.className = 'output container';
-            this._output.innerHTML = `<p>Стоимость всех товаров ${basket.findPriceGoods()}</p>`;
+            this._output.innerHTML = `<p>Стоимость всех товаров <span id="total-price">${basket.findPriceGoods()}</span></p>`;
             // console.log(this._output);
             return this._output;
             // catalog.insertAdjacentHTML('afterend', `${this._output}`);
         } else {
-            document.querySelector('.output').innerHTML = `<p>Стоимость всех товаров ${basket.findPriceGoods()}</p>`;
+            document.querySelector('#total-price').innerHTML = `${basket.findPriceGoods()}`;
         }
     }
 
+    //подсчет общей стоимости товаров в корзине
     findPriceGoods(){
         let summ = null;
         this.basket.forEach((item) => {
@@ -123,23 +179,60 @@ class ItemBasket extends  Item{
         super(title, price, image, id,);
         this.quantity = quantity;
     }
+
+    //отрисовывает товары из корзины на странице
     render() {
         return `<div data-art="${this.id}" class="basket__item bxbb"> <img src="image/${this.img}" 
             alt="photo"><h3>${this.title}</h3><p>цена: ${this.price}р</p>
-            <div class="button basket_button">+</div><div class="quantity_out">${this.quantity}</div><div class="button basket_button">-</div></div>` ;
+            <div class="button basket_button add">+</div><div class="quantity_out">${this.quantity}</div><div class="diminish button basket_button">-</div></div>` ;
 
     }
 }
-const basket = new BasketList();
 
+//создание корзины, запрос содержимого корзины с сервера
+const basket = new BasketList();
 const basketOut = document.querySelector('#basket');
-function test2(){
-    for (const key of items.items) {
-        if (+this.parentNode.dataset.art === key.id) {
-            basketOut.innerHTML = basket.checkItems(key.id);
-            basket.outResult();
-            basketOut.after(basket._output);
-            // console.log(basket._output)
+basket.fetchBasket().then(() => {
+    if (basket !== []) {
+        basketOut.innerHTML = basket.render();
+        basket.outResult();
+        basketOut.after(basket._output);
+    }
+});
+
+//слушатель событий в блоке #basket на кнопки "+" и "-"
+document.getElementById('basket').addEventListener('click', (event) => {
+    if (event.target.classList.contains('add')){
+        for (const key of items.items) {
+            if (+event.target.parentNode.dataset.art === key.id) {
+                basket.checkItems(key);
+                basket.outResult();
+                basketOut.after(basket._output);
+            }
+        }
+    } else if (event.target.classList.contains('diminish')){
+        for (const key of items.items) {
+            if (+event.target.parentNode.dataset.art === key.id) {
+                const subtotal = basket.addQuantityDiminish(key.id);
+                if (subtotal !== false) {
+                    basketOut.innerHTML = subtotal;
+                    basket.outResult();
+                    basketOut.after(basket._output);
+                }
+            }
         }
     }
-}
+});
+
+//слушатель событий в блоке .catalog на кнопку "добавить"
+document.querySelector('.catalog').addEventListener('click', (event) => {
+        if (event.target.classList.contains('catalog_button')) {
+            for (const key of items.items) {
+                if (+event.target.parentNode.dataset.art === key.id) {
+                    basket.checkItems(key);
+                    basket.outResult();
+                    basketOut.after(basket._output);
+                }
+            }
+        }
+    });
